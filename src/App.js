533290160c65582f59460b7d06ad692979fc6bc1
Brian <bstanton773@gmail.com>
2022-09-01 15:59:34 -0500

    Add Alert Messaging

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Racers from './components/Racers';
 import RacersClass from './components/RacersClass';
 import { Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
+import AlertMessage from './components/AlertMessage';
+import { useState } from 'react';
 
 function App(props) {
-    console.log('Hello')
+    const [message, setMessage] = useState(null);
+    const [category, setCategory] = useState(null);
+
+    const flashMessage = (message, category) => {
+        setMessage(message);
+        setCategory(category)
+    }
+
     return (
         <>
             <Navbar name='Brian' city='Chicago'/>
             <div className='container'>
+                {message ? <AlertMessage message={message} category={category} flashMessage={flashMessage} /> : null}
                 <Routes>
                     <Route path='/' element={<ButtonCounter />} />
                     <Route path='/standings' element={<RacersClass />} />
-                    <Route path='/register' element={<Register />} />
+                    <Route path='/register' element={<Register flashMessage={flashMessage}/>} />
                 </Routes>
             </div>
         </>
